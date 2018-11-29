c5ffd66fdd81572adbfb75d248bc583004a9c7d8
sroziewski <szymon.roziewski@gmail.com>
2018-11-29 16:32:26 +0100

    redirecting with searching, query Param

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -29,7 +29,7 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes,{
     enableTracing: true,
-    useHash: true,
+    // useHash: true,
   })],
   exports: [RouterModule]
 })
--- src/app/music/components/music-search/music-search.component.ts
+++ src/app/music/components/music-search/music-search.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Album } from 'src/app/model/Albums';
 import { MusicSearchService } from '../../services/music-search.service';
 import { catchError, tap } from 'rxjs/operators';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-music-search',
@@ -21,12 +22,31 @@ export class MusicSearchComponent implements OnInit {
   message: string;
   query$ = this.service.getQuery()
 
-  constructor(private service: MusicSearchService) { }
+  constructor(
+    private service: MusicSearchService,
+    private route: ActivatedRoute,
+    private router: Router
+    ) { }
 
   search(query: string){
-    this.service.search(query)
+    this.service.search(query);
+    this.router.navigate(["/music"], {
+      queryParams: {
+        q:query
+      },
+      // replaceUrl: true
+    })
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    // const query = this.route.snapshot.queryParamMap.get('q')
+    this.route.queryParamMap.subscribe(queryParamMap=>{
+      const query = queryParamMap.get('q');
+      if(query){
+        this.search(query)
+      }
+    })
+
+  }
 
 }
