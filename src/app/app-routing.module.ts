a44ce29c1c9ceb4cf3e84797f27742d3ad87ac9f
sroziewski <szymon.roziewski@gmail.com>
2018-11-29 16:12:08 +0100

    navi panel added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PlaylistsViewComponent } from './playlists/components/playlists-view/playlists-view.component';
+import { MusicSearchComponent } from './music/components/music-search/music-search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo: 'playlist',
+    pathMatch: "full"
+  },
+  {
+    path:'playlist',
+    component: PlaylistsViewComponent
+  },
+  
+  {
+    path:'music',
+    component: MusicSearchComponent
+  },
+  
+  {
+    path:'**',
+    redirectTo: 'playlist',
+    pathMatch: "full"
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{
+    enableTracing: true,
+    useHash: true,
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
